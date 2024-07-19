fd5c3e9f986f6d8bbb898bdb6047c96279ed2c40
aufall02 <aufaluzumaki@gmail.com>
2024-07-19 07:01:24 +0700

    chore : testing screen

--- src/Screens/Main/HomeScreen.tsx
+++ src/Screens/Main/HomeScreen.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { Button, Layout, Text } from '@ui-kitten/components';
+import { Button, Layout, Text,List, ListItem  } from '@ui-kitten/components';
 import auth from '@react-native-firebase/auth';
 
 const handleLogin = () => {
-    auth()
-  .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-  .then(() => {
-    console.log('User account created & signed in!');
-  })
-  .catch(error => {
-    if (error.code === 'auth/email-already-in-use') {
-      console.log('That email address is already in use!');
-    }
-
-    if (error.code === 'auth/invalid-email') {
-      console.log('That email address is invalid!');
-    }
-
-    console.error(error);
-  });
+    console.log('kepencet')
 }
 
 const HomeScreen: React.FC = () => {
--- src/Screens/Main/ReportScreen.tsx
+++ src/Screens/Main/ReportScreen.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Layout, Text } from '@ui-kitten/components';
+import { ScrollView , StyleSheet} from 'react-native';
+
 
 const ReportScreen: React.FC = () => {
     return (
         <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text category='h1'>Report</Text>
+            <ScrollView style={styles.scrollView}>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+            </ScrollView>
         </Layout>
     )
 };
+
+const styles = StyleSheet.create({
+    scrollView: {
+      marginHorizontal: 10,
+    },
+    text: {
+      fontSize: 12,
+    },
+  });
 
 export default ReportScreen
