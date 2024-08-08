210f01264f6fd1028b672f610a0ba737bb287375
Aufal marom <91175425+aufall02@users.noreply.github.com>
2024-08-08 11:16:15 +0700

    feat : selected field in gender

--- src/Screens/Auth/RegisterScreen.tsx
+++ src/Screens/Auth/RegisterScreen.tsx
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
-import {Text, CheckBox, Layout, RadioGroup, Radio} from '@ui-kitten/components';
 import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  ToastAndroid,
-  View,
-} from 'react-native';
+  Button,
+  Text,
+  CheckBox,
+  Input,
+  Layout,
+  Select,
+  SelectItem,
+  IndexPath,
+} from '@ui-kitten/components';
+import {Alert, Image, ScrollView, StyleSheet, ToastAndroid} from 'react-native';
 import FormInput from '../../components/FormInput';
 import useForm from '../../helpers/useFormHooks';
 import {handleZodError, validateUser} from '../../helpers/validateUser.ts';
 import {Logout, SignUpWithEmailAndPassword} from '../../service';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import ButtonCompo from '../../components/ButtonCompo.tsx';
+import GenderSelect from "../../components/GenderSelect.tsx";
 
 const initialState = {
   nama_lengkap: '',
@@ -28,9 +32,13 @@ const initialState = {
 
 export default function RegisterScreen() {
   const {formData, handleInputChange, errors, setFieldError, clearFieldError} =
-    useForm(initialState);
+      useForm(initialState);
   const navigation = useNavigation<NavigationProp<any>>();
+  // const [selectedIndex, setSelectedIndex] = useState<IndexPath | IndexPath[]>(
+  //     new IndexPath(0),
+  // );
   const [isChecked, setIsChecked] = useState(false);
+  // console.log(selectedIndex.row)
 
   const validateForm = () => {
     Object.keys(formData).forEach(field => clearFieldError(field));
@@ -45,14 +53,17 @@ export default function RegisterScreen() {
     return true;
   };
 
+  console.log(formData)
+
   const handleRegister = async () => {
+    // formData['gender']=selectedIndex.row;
     if (validateForm()) {
       const user = validateUser(formData);
       if (user.success) {
         const newUser = await SignUpWithEmailAndPassword(
-          user.data!,
-          user.data!.password!,
-          user.data!.confirm_password!,
+            user?.data!,
+            user?.data!.password!,
+            user?.data!.confirm_password!,
         );
         if (newUser?.success) {
           await Logout();
@@ -66,98 +77,69 @@ export default function RegisterScreen() {
   };
 
   return (
-    <Layout>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View
-          style={{
-            flexDirection: 'row',
-            marginBottom: 20,
-          }}>
-          <Image
-            source={require('../../assets/img/logo1.png')}
-            style={{
-              width: 100,
-              height: 100,
-              resizeMode: 'contain',
-            }}
-          />
-          <Image
-            source={require('../../assets/img/logo.png')}
-            style={{
-              width: 100,
-              height: 100,
-              resizeMode: 'contain',
-            }}
-          />
-          <Image
-            source={require('../../assets/img/logo2.png')}
-            style={{
-              width: 100,
-              height: 100,
-              resizeMode: 'contain',
-            }}
-          />
-        </View>
-        <Layout style={styles.form}>
-          {[
-            'nama_lengkap',
-            'email',
-            'usia',
-            'kelas',
-            'asal_sekolah',
-            'gender',
-            'no_ortu',
-            'alamat_lengkap',
-          ].map(field => (
+      <Layout>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Image source={require('../../assets/img/logo.png')} />
+          <Layout style={styles.form}>
+            {[
+              'nama_lengkap',
+              'email',
+              'usia',
+              'kelas',
+              'asal_sekolah',
+              'no_ortu',
+              'alamat_lengkap',
+            ].map(field => (
+                <FormInput
+                    key={field}
+                    label={field
+                        .replace(/_/g, ' ')
+                        .replace(/([A-Z])/g, ' $1')
+                        .trim()}
+                    placeholder=""
+                    value={formData[field]}
+                    onChangeText={value => handleInputChange(field, value)}
+                    status={errors[field] ? 'danger' : 'basic'} // Set status based on error
+                    error={errors[field] || null}
+                />
+            ))}
+            <GenderSelect onGenderChange={gender => handleInputChange('gender', gender)} />
             <FormInput
-              key={field}
-              label={field
-                .replace(/_/g, ' ')
-                .replace(/([A-Z])/g, ' $1')
-                .trim()}
-              placeholder=""
-              value={formData[field]}
-              onChangeText={value => handleInputChange(field, value)}
-              status={errors[field] ? 'danger' : 'basic'} // Set status based on error
-              error={errors[field] || null}
+                label="Password"
+                placeholder=""
+                value={formData.password}
+                onChangeText={value => handleInputChange('password', value)}
+                status={errors.password ? 'danger' : 'basic'}
+                error={errors.password} // Pass error message
+                secureTextEntry
             />
-          ))}
-          <FormInput
-            label="Password"
-            placeholder=""
-            value={formData.password}
-            onChangeText={value => handleInputChange('password', value)}
-            status={errors.password ? 'danger' : 'basic'}
-            error={errors.password} // Pass error message
-            secureTextEntry
-          />
-          <Text style={styles.label}>Minimum 8 characters</Text>
-          <FormInput
-            label="Confirm Password"
-            placeholder=""
-            value={formData.confirm_password}
-            onChangeText={value => handleInputChange('confirm_password', value)}
-            status={errors.confirm_password ? 'danger' : 'basic'}
-            error={errors.confirm_password} // Pass error message
-            secureTextEntry
-          />
-          <Text style={styles.label}>Minimum 8 characters</Text>
-          <CheckBox
-            style={styles.checkbox}
-            checked={isChecked}
-            onChange={setIsChecked}>
-            Saya Setuju dengan ketentuan diatas
-          </CheckBox>
-          <ButtonCompo
-            status="primary"
-            width={300}
-            onPress={handleRegister}
-            disabled={!isChecked}
-            text="Daftar"
-          />
-        </Layout>
-      </ScrollView>
-    </Layout>
+            <Text style={styles.label}>Minimum 8 characters</Text>
+            <FormInput
+                label="Confirm Password"
+                placeholder=""
+                value={formData.confirm_password}
+                onChangeText={value => handleInputChange('confirm_password', value)}
+                status={errors.confirm_password ? 'danger' : 'basic'}
+                error={errors.confirm_password} // Pass error message
+                secureTextEntry
+            />
+            <Text style={styles.label}>Minimum 8 characters</Text>
+            <CheckBox
+                style={styles.checkbox}
+                checked={isChecked}
+                onChange={setIsChecked}>
+              Saya Setuju dengan ketentuan diatas
+            </CheckBox>
+            <ButtonCompo
+                status="primary"
+                width={300}
+                onPress={handleRegister}
+                disabled={!isChecked}
+                text="Daftar"
+            />
+          </Layout>
+        </ScrollView>
+      </Layout>
   );
 }
 
---
+++ src/components/GenderSelect.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import { IndexPath, Select, SelectItem } from '@ui-kitten/components';
+
+interface GenderSelectProps {
+    onGenderChange: (gender: 'male' | 'female') => void;
+}
+
+const GenderSelect: React.FC<GenderSelectProps> = ({ onGenderChange }) => {
+    const [selectedIndex, setSelectedIndex] = useState<IndexPath>(new IndexPath(0));
+
+    const handleSelect = (index: IndexPath) => {
+        setSelectedIndex(index);
+        const gender = index.row === 1 ? 'male' : 'female';
+        onGenderChange(gender);
+    };
+
+    let gender
+    switch (selectedIndex.row) {
+        case 0:
+            gender = 'Pilih Gender'
+            break
+        case 1:
+            gender = 'Laki - Laki'
+            break
+        case 2:
+            gender= 'Perempuan'
+            break
+        default:
+            gender='Pilih Gender'
+    }
+
+    return (
+        <Select
+            label={() => (
+                <Text style={{ marginStart: 10, marginTop: 10, color: 'grey', paddingVertical: 5 }}>
+                    Gender
+                </Text>
+            )}
+            selectedIndex={selectedIndex}
+            onSelect={handleSelect}
+            // value={selectedIndex.row === 1 ? 'Laki-laki' : 'Perempuan'}
+            value={gender}
+        >
+            <SelectItem title="Pilih gender" />
+            <SelectItem title="Laki-laki" />
+            <SelectItem title="Perempuan" />
+        </Select>
+    );
+};
+
+export default GenderSelect;
