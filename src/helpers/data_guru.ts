7e209a77a33285f2387427ea5253c6950fca0a89
Aufal marom <91175425+aufall02@users.noreply.github.com>
2024-08-10 11:00:14 +0700

    chore : setup account guru dan perbaikan bug akun

---
+++ src/helpers/data_guru.ts
@@ -0,0 +1,14 @@
+export const guru_smp = [
+    'aufaluzumaki@gmail.com',"sahrul.tayadih@gmail.com",
+    "sahrul.canva.bebas@gmail.com",
+   "wongtulus360@gmail.com",
+    'koyekkasyi@gmail.com'
+]
+
+
+export const guru_sma = [
+    'aufaluzumaki@gmail.com',"sahrul.tayadih@gmail.com",
+    "sahrul.canva.bebas@gmail.com",
+    "wongtulus360@gmail.com",
+    'koyekkasyi@gmail.com'
+]
